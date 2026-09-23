6b2e295a932c354e1fd080b8662fa5e0507f767a
rajasekhar6739 <speed6739s@gmail.com>
2026-09-23 23:04:21 +0530

    Add API URL from environment variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-
+const API = import.meta.env.VITE_API_URL;
 function App() {
   const [task, setTask] = useState("");
   const [result, setResult] = useState("");
